55ccabc042a9e4c1329e97df99aafc8284ec479d
Soumya7077 <110816458+Soumya7077@users.noreply.github.com>
2023-09-28 17:54:04 +0530

    Fist Commit

--- server/server.js
+++ server/server.js
@@ -913,7 +913,10 @@ app.get("/getCategorywisePackage/:type", async (req, res) => {
 app.post("/addpromotion", async (req, res) => {
   try {
     // Find the last promotion to determine the new ID
-    const lastPromotion = await promotionManage.find({}, { sort: { id: -1 } });
+    const lastPromotion = await promotionManage
+    .findOne({}, { sort: { id: -1 } })
+    .select("id")
+    .lean();
     const newId = lastPromotion ? lastPromotion.id + 1 : 1;
 
     console.log("=============================================", lastPromotion);
